f6f15bcaec9c906e4a6643a7587ed244380b24e8
py <pyd3.14@gmail.com>
2022-05-25 13:38:12 +0200

    smooth scroll to top

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -152,8 +152,7 @@ var app = {
     },
 
     scrollToTop: function () {
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
+        window.scrollTo({ top: 0, behavior: 'smooth' });
     },
 
     searchCat: function (evt) {
